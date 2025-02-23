5086201a342e484fb289c07d1a4948005966683f
masterujjval <guptarickey3@gmail.com>
2025-02-23 15:03:05 +0530

    [Ujjwal Gupta] Add: base code added

---
+++ payrollapp/src/main/java/com/payroll/payrollapp/PayrollappApplication.java
@@ -0,0 +1,13 @@
+package com.payroll.payrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollappApplication.class, args);
+	}
+
+}
---
+++ payrollapp/src/test/java/com/payroll/payrollapp/PayrollappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.payrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
